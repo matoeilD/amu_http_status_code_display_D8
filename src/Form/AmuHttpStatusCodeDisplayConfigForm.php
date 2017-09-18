434c2b4584da928a750a307163d3d39639581f62
mathieu Dandonneau <mathieu.dandonneau@univ-amu.fr>
2017-09-18 10:29:56 +0200

    404 and 403 hook content done

---
+++ src/Form/AmuHttpStatusCodeDisplayConfigForm.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\amu_http_status_code_display\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class AmuHttpStatusCodeDisplayConfigForm extends ConfigFormBase {
+//SiteInformationForm
+
+  public function getFormId() {
+    return 'AmuHttpStatusCodeDisplayConfigForm';
+  }
+
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config('amustatuscode.settings');
+
+
+    $form = parent::buildForm($form, $form_state);
+
+    $form['404_message'] = array(
+      '#type' => 'textarea',
+      '#title' => t('404 error message'),
+      '#description' => t('error 404 message.'),
+      '#default_value' => '404 error',
+    );
+
+    $form['403_message'] = array(
+      '#type' => 'textarea',
+      '#title' => t('403 error message'),
+      '#description' => t('error 403 message.'),
+      '#default_value' => '403 error',
+    );
+
+    return $form;
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $config = $this->config('amustatuscode.settings');
+    $config->set('404_message', $form_state->getValue('404_message'));
+    $config->set('403_message', $form_state->getValue('403_message'));
+    $config->save();
+
+    return parent::submitForm($form, $form_state);
+
+  }
+
+  public function getEditableConfigNames() {
+    return ['amustatuscode.settings'];
+  }
+
+}
