7d8fae6698d5733186e533819d3ae1d92fea1714
mathieu Dandonneau <mathieu.dandonneau@univ-amu.fr>
2017-09-18 10:31:44 +0200

    removing routesubscriber

--- src/Form/AmuHttpStatusCodeDisplayConfigForm.php
+++ src/Form/AmuHttpStatusCodeDisplayConfigForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 class AmuHttpStatusCodeDisplayConfigForm extends ConfigFormBase {
-//SiteInformationForm
+
 
   public function getFormId() {
     return 'AmuHttpStatusCodeDisplayConfigForm';
